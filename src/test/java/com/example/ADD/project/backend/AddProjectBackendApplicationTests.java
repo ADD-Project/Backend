5ed88cf413f807e3f42164b2f7d8b2f75e53909d
ynghan <wjddudgks5669@gmail.com>
2026-04-05 17:34:02 +0900

    ADD project start

---
+++ src/test/java/com/example/ADD/project/backend/AddProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ADD.project.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
